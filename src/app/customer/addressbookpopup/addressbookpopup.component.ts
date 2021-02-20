9917e6461afd9333c21bb45b6825d373343ee292
riya-3601 <palkhiwala.riya@yahoo.com>
2021-02-20 13:15:45 +0530

    logout button added

--- src/app/customer/addressbookpopup/addressbookpopup.component.ts
+++ src/app/customer/addressbookpopup/addressbookpopup.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { OrderdetailsService } from 'src/app/orderdetails.service';
 import { AddressbookService } from "src/app/addressbook.service";
+
 @Component({
   selector: 'app-addressbookpopup',
   templateUrl: './addressbookpopup.component.html',
@@ -12,11 +13,11 @@ export class AddressbookpopupComponent implements OnInit {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,private _address:AddressbookService) { }
 
   ngOnInit(): void {
-    this._address.get(this.data).subscribe(
-      (data:Orddet)=>{
-        console.log(data);
-      }
-    );
+    // this._address.get(this.data).subscribe(
+    //   (data:Orddet)=>{
+    //     console.log(data);
+    //   }
+    // );
 
   }
 
